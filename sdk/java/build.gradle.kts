e8c2339c69c7b8f2ffdec6e633778dfcf6dd41a0
Shukant Pal <shukant@impala.systems>
2026-08-08 21:16:12 -0700

    Bump SDK packages to 0.1.18 (#257)

--- sdk/java/build.gradle.kts
+++ sdk/java/build.gradle.kts
@@ -10,7 +10,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "systems.impala"
-    version = "0.1.17"
+    version = "0.1.18"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_17
