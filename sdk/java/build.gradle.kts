ace4247db9ac747fae25de8dafd3f20e501c0630
Shukant Pal <SukantK2002@outlook.com>
2026-08-21 22:06:14 -0700

    Bump Talon SDK packages to 0.2.9

--- sdk/java/build.gradle.kts
+++ sdk/java/build.gradle.kts
@@ -10,7 +10,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "systems.impala"
-    version = "0.2.8"
+    version = "0.2.9"
 
     java {
         sourceCompatibility = JavaVersion.VERSION_17
